d13af4db427dc7e68d87f212facf58fb094dc76a
Lilian Almeida <lili.l2002@hotmail.com>
2023-04-20 17:13:50 -0300

    enum and functions

--- build/types.js
+++ build/types.js
@@ -1,3 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.CATEGORY = void 0;
+var CATEGORY;
+(function (CATEGORY) {
+    CATEGORY["STREAMING"] = "Servi\u00E7o de Streaming";
+    CATEGORY["LIVETV"] = "Tv ao Vivo";
+    CATEGORY["OPENTV"] = "Tv Aberta";
+})(CATEGORY = exports.CATEGORY || (exports.CATEGORY = {}));
+;
 //# sourceMappingURL=types.js.map
--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,4 @@
-import { Product, Purchase, User } from "./types";
+import { Product, Purchase, User, CATEGORY } from "./types";
 
 export const users: User[] = [
     {
@@ -13,22 +13,71 @@ export const users: User[] = [
     }
     
   ];
+
+  export function createUser(id: string, email: string, password: string): void {
+
+    const newUser: User = {
+        id,
+        email,
+        password
+    }
+    users.push(newUser)
+    console.log("Novo usuário cadastrado com sucesso!")
+
+}
+
+export function getAllUsers(): User[] {
+
+  return users
+}
   
   export const products: Product[] = [
     {
       id: "P1",
       name: "HBO Max",
       price: 27,
-      category: "Serviço de Streaming",
+      category: CATEGORY.STREAMING,
     },
     {
       id: "P2",
       name: "Prime Video",
       price: 15,
-      category: "Serviço de Streaming",
+      category: CATEGORY.STREAMING,
     }
 
   ];
+
+  export function getAllProducts(): Product[] {
+
+    return products
+}
+
+export function createProduct(id: string, name: string, price: number, category: CATEGORY): void {
+
+    const newProduct: Product = {
+        id,
+        name,
+        price,
+        category
+    }
+    products.push(newProduct)
+    console.log("Produto cadastrado com sucesso!")
+
+}
+
+
+
+export function getProductById(id: string): Product[] {
+    return products.filter((item) => {
+        return item.id === id
+    })
+
+}
+
+export function queryProductsByName(name: string): Product | undefined {
+    return products.find((item) => { return item.name.toLowerCase().includes(name.toLowerCase()) })
+
+}
   
   export const purchase: Purchase[] = [
     {
@@ -44,3 +93,25 @@ export const users: User[] = [
       totalPrice: 15,
     },
   ];
+
+  export function createPurchase(userId: string, productId: string, quantity: number, totalPrice: number): void {
+
+    const newPurchase: Purchase = {
+        userId,
+        productId,
+        quantity,
+        totalPrice
+    }
+    purchase.push(newPurchase)
+    console.log("Compra realizada com sucesso!")
+
+}
+
+export function getAllPurchasesFromUserId(id: string): Purchase[] {
+    return purchase.filter((item) => { return item.userId === id })
+}
+
+
+
+
+const result = purchase.reduce((acc, current) => acc + (current.quantity * current.totalPrice), 0)
